b4bb80e7273aab2d74e13834cdd4d8ae1ae5b73a
Svetlana Alexeyenko <sv.alexeyenko@gmail.com>
2018-09-23 23:05:35 +0300

    add nav-block

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,6 +1,19 @@
-//Some JS
-let a='Hello';
-let b='World';
-[a, b] = [b, a];
+var callBtns = $(".js_call-btn");
+var hiddenBlocks = $(".js_hidden-block");
+callBtns.click(function(e) {
+  e.preventDefault();
+  //Зупинити вспливання події
+  e.stopPropagation();
+  //   Знайти найближчий блок по селектору
+  var target = $(this).siblings(".js_hidden-block");
+  target.toggleClass("is-active");
+  hiddenBlocks.not(target).removeClass("is-active");
+});
 
-console.log(`${a} ${b}`);
+$(document).click(function(e) {
+  hiddenBlocks.removeClass("is-active");
+});
+
+hiddenBlocks.click(function(e) {
+  e.stopPropagation();
+});
